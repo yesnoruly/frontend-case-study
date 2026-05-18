b0885fdaef5ce82c4611e72ec9524d5388131105
yesnoruly <karvatskij.roma@gmail.com>
2026-05-18 16:22:11 +0200

    feat: add google calendar link

--- src/components/Aside.tsx
+++ src/components/Aside.tsx
@@ -1,14 +1,26 @@
 import { Button } from "./ui/button";
 import { Skeleton } from "./ui/skeleton";
 
+import { google } from 'calendar-link'
+
 import { $event } from "./model/event";
 
 import { useUnit } from "effector-react";
+
+type TCalendarEvent = {
+    title: string,
+    start: string,
+    uid?: string,
+    description?: string,
+    end?: string,
+    location?: string,
+}
 
 export const Aside = () => {
 
     const event = useUnit($event);
 
+    // Skeleton
     if (!event) {
         return (
             <aside className="w-full max-w-sm bg-gray-100 rounded-md shadow-sm p-3 flex flex-col gap-2">
@@ -16,9 +28,20 @@ export const Aside = () => {
                 <Skeleton className="h-6 w-3/4" />
                 <Skeleton className="h-4 w-full" />
                 <Skeleton className="h-4 w-full" />
-            </aside> 
+            </aside>
         )
     }
+
+    const calendarEvent = {
+        uid: event?.eventId,
+        title: event?.namePub,
+        description: event?.description,
+        start: event?.dateFrom,
+        end: event?.dateTo,
+        location: event?.place,
+    } as TCalendarEvent
+
+    const googleUrl = google(calendarEvent)
 
     return (
         <aside className="w-full max-w-sm bg-gray-100 rounded-md shadow-sm p-3 flex flex-col gap-2">
@@ -35,8 +58,8 @@ export const Aside = () => {
                 {event?.dateFrom ? new Date(event.dateFrom).toLocaleString('cs-CZ') : ''} - {event?.dateTo ? new Date(event.dateTo).toLocaleString('cs-CZ') : ''}
             </p>
             {/* add to calendar button */}
-            <Button variant="secondary">
-                Add to calendar
+            <Button variant="default" className="mt-2">
+                <a href={googleUrl} target="_blank" rel="noopener noreferrer">Add to calendar</a>
             </Button>
         </aside>
     )
